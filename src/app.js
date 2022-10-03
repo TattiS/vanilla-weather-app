f113bfb6dc35f2df152703bd762ad2c876bd6614
TattiS <tanyas81@gmail.com>
2022-10-03 10:41:33 +0300

    added forecast

--- src/app.js
+++ src/app.js
@@ -26,6 +26,56 @@ function writeDate() {
     }`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function setForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  console.log(forecast);
+  let forecastHTML = `<div class="row" id="forecast-row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index > 0 && index < 7) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(lat, lon) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(setForecast);
+}
+
 function findByCity(event) {
   let cityInput = document.querySelector("#city-input");
   let city = document.querySelector("#city");
@@ -49,6 +99,7 @@ function findByCity(event) {
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
     icon.setAttribute("alt", response.data.weather[0].description);
+    getForecast(response.data.coord.lat, response.data.coord.lon);
   });
 }
 
@@ -74,6 +125,7 @@ function setCurrentPosition(event) {
         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
       );
       icon.setAttribute("alt", response.data.weather[0].description);
+      getForecast(response.data.coord.lat, response.data.coord.lon);
     });
   });
 }
